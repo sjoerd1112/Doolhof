fe33ccb5f84177da6b2c6c1327efdb1bc6c293cf
timothy-b1997 <48526044+timothy-b1997@users.noreply.github.com>
2019-03-15 17:26:20 +0100

    Add files via upload

--- src/Doolhof/components/label.java
+++ src/Doolhof/components/label.java
@@ -3,13 +3,9 @@ package Doolhof.components;
 import javax.swing.*;
 import java.awt.*;
 
-public class label extends JComponent{
-    public static void main(String[] args) {
-        JLabel label = new JLabel();
-        label.setSize(60,60);
+public class label {
+
+    public label () {
     }
 
-    public void paintComponent(Graphics g){
-
-    }
 }
